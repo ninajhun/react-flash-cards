36ed290d99fef58b446c4f5e62f7781a3562f886
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-12-19 07:26:47 +0000

    add previousCard method

--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -8,21 +8,34 @@ class ReviewCards extends React.Component {
       isQuestionSide: true
     };
     this.index = 0;
-    this.nextCard = this.nextCard.bind(this)
+    this.nextCard = this.nextCard.bind(this);
+    this.previousCard = this.previousCard.bind(this)
   }
 
   componentDidMount(){
-    this.context.setActiveCard(0)
+    this.context.setActiveCard(0) //or this.index?
   }
 
   nextCard(){
     if (this.index > this.context.cards.length - 1) {
-      this.index = 0
+      this.index = 0  // bug last card to first is undefined
     }
-    this.context.setActiveCard(this.index)
     this.index++
+    this.context.setActiveCard(this.index)
   }
 
+  previousCard(){
+    if(this.index === 0){
+      this.index = this.context.cards.length;
+      this.context.setActiveCard(this.index)
+    }
+    this.index--
+    this.context.setActiveCard(this.index)
+
+
+
+
+  }
 
 
 
@@ -34,7 +47,9 @@ class ReviewCards extends React.Component {
             <h1 className="text-center">Review Cards</h1>
           </div>
           <div className="row">
+            <button type="button" className="btn btn-primary" onClick={this.previousCard}>Test previousCard </button>
             <button type="button" className="btn btn-primary" onClick= {this.nextCard}>Test Next Card</button>
+
           </div>
         </div>
 
