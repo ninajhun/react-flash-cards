970810c702c825b7a02259cf95edb04e17398e39
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-12-19 06:52:16 +0000

    on component mount set activeCard to first card, add property to state to determine which side is displayed

--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -1,9 +1,25 @@
 import React from "react"
+import { AppContext } from "./context"
 
 class ReviewCards extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isQuestionSide: true
+    }
+  }
+
+  componentDidMount(){
+    this.context.setActiveCard(0)
+  }
+
+
+
+
   render() {
     return <h1 className="text-center">Review Cards</h1>
   }
 }
 
+ReviewCards.contextType = AppContext
 export default ReviewCards
